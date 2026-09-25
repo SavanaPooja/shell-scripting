d6e5e03e023a396d20f4b525025f26e68c5190e4
basita512 <jeffxook1012@gmail.com>
2026-09-25 12:12:32 +0530

    Variable

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Mamatha
+PERSON2=Sunitha
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am good"
+echo "$PERSON1:: Ha $PERSON2 I want to tell you something"
+echo "$PERSON2:: yes, plese $PERSON1"
